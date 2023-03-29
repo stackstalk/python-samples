1cc9990b2a003a12f6c2a5cb44734e7ed9294bd1
stackstalk <stackstalk83@gmail.com>
2023-03-29 19:27:34 +0530

    Added pipelines artifacts

--- fastapi-hello/app/main.py
+++ fastapi-hello/app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI 
+
+import uvicorn
+
 app = FastAPI() 
 
-@app.get("/") 
+@app.get("/hello") 
 async def root(): 
      return {"message": "Hello, World!"} 
+
+@app.get("/health") 
+async def root(): 
+     return "ok" 
+
+if __name__ == '__main__':
+    uvicorn.run('main:app',
+                host='0.0.0.0',
+                port=9089,
+                reload=True)
